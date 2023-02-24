8c74136b22233aae26fe3b3aaf6f4c74e2f99357
rajpriyaa <124484970+rajpriyaa@users.noreply.github.com>
2023-02-24 14:02:56 +0530

    Update ArraySplit.java

--- AssignmentQuestion/ArraySplit.java
+++ AssignmentQuestion/ArraySplit.java
@@ -1,48 +1,37 @@
 package AssignmentQuestion;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
 public class ArraySplit {
-    public static Integer sum(List<Integer> list) {
-        int s=0;
-        for(int i=0;i<list.size();i++) {
-            s+=list.get(i);
-        }
-        return s;
-    }
-    public static void sortMyArray(Integer[] input) {
-        List<Integer> list=Arrays.asList(input);
-        List<Integer> selectedlist1=new ArrayList<>();
-        List<Integer> selectedlist2=new ArrayList<>();
-        for(Integer i:list) {
-            if(sum(selectedlist1)<sum(selectedlist2) && selectedlist1.size()<=list.size()/2) {
-                selectedlist1.add(i);
-            }else {
-                selectedlist2.add(i);
-            }
-        }
-        List<Integer> list1=new ArrayList<>();
-        list1.addAll(selectedlist1);
-        list1.addAll(selectedlist2);
-        System.out.println(list1);
-
-        System.out.println(Math.abs(sum(selectedlist1)-sum(selectedlist2)));
-    }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-//         System.out.println("Enter the size of the list");
-        int n=sc.nextInt();
-        Integer[] arr=new Integer[n];
-        for(int i=0;i<n;i++) {
-            arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
         sc.close();
-        Arrays.sort(arr,Collections.reverseOrder());
-        sortMyArray(arr);
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int sum1 = 0, sum2 = 0;
+
+        for (Integer num : arr) {
+            if (sum1 <= sum2) {
+                list1.add(num);
+                sum1 += num;
+            } else {
+                list2.add(num);
+                sum2 += num;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(list1);
+        list.addAll(list2);
+
+        System.out.println("Sorted Array: " + list);
+//        we can print the sum diff between the list tpp using Math.abs.
+//        System.out.println("Absolute Difference: " + Math.abs(sum1 - sum2));
     }
 }
