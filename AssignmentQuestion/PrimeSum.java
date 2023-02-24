c615398a63fb0316a2a5f13453e5523b38b8ad18
rajpriyaa <124484970+rajpriyaa@users.noreply.github.com>
2023-02-24 14:01:55 +0530

    Update PrimeSum.java

--- AssignmentQuestion/PrimeSum.java
+++ AssignmentQuestion/PrimeSum.java
@@ -1,40 +1,45 @@
 package AssignmentQuestion;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrimeSum {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        int n=2000000000;
-        boolean [] isPrime = new boolean[n];
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        s = sc.nextLine();
+
+        BigInteger n = new BigInteger(s);
+        boolean [] isPrime = new boolean[n.intValue() + 1];
 
         allPrimes(isPrime,n);
 
-
-
-        int m = sc.nextInt();
-        for(int i=0;i<m;i++){
-            if(isPrime[i] && isPrime[m-i]){
-                System.out.println(i+" "+(m-i));
+        for(int i=2;i<=n.intValue();i++){
+            if(isPrime[i] && isPrime[n.intValue()-i]){
+                System.out.println(n.intValue()+" = "+i+" + "+(n.intValue()-i));
+                break;
             }
         }
     }
 
-    static void allPrimes(boolean [] isPrime, int n){
+    static void allPrimes(boolean [] isPrime, BigInteger n){
 
         Arrays.fill(isPrime,true);
 
         isPrime[0]=isPrime[1]=false;
 
-        for(int i=2;i<=Math.sqrt(n);i++) {
+//        for(int i=2;i<=n.intValue();i++) {
 
+                for(int i=2;i<=Math.sqrt(n.intValue());i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                for (int j = i * i; j <= n.intValue(); j += i) {
                     isPrime[j] = false;
                 }
             }
         }
 
-    }}
+    }
+}
